99bd84ff3f1abd406cd13204a6246c1a58226d50
Risko Ruus <ruus@rushstreetinteractive.com>
2019-11-07 20:13:26 +0200

    Clearing cookies before test

--- cypress/integration/ut2019.spec.js
+++ cypress/integration/ut2019.spec.js
@@ -1,8 +1,9 @@
 describe('ut2019', function() {
 	const email = 'test.user@ut2019.ee';
-	const name = 'Martin Aun';
+	const name = 'Test User';
 	const message = 'Custom message!';
 	it('Submit form successfully', function() {
+		cy.clearCookies();
 		cy.visit('https://ut2019.voog.com/contact');
 		cy.get('.contact-name input').type(name);
 		cy.get('.contact-email input').type(email);
