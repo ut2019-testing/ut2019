a23b61addade67e7dcdd3f5a3fa3b8f0b3d89376
Anneli Klamas <annelik98@gmail.com>
2019-11-13 23:30:52 +0200

    edited title

--- cypress/integration/test2.spec.js
+++ cypress/integration/test2.spec.js
@@ -1,7 +1,7 @@
 describe('ut2019', function() {
     const languages = ['https://ut2019.voog.com/contact', 'https://ut2019.voog.com/contact-1'];
     const reply = ['Invalid input', 'Vigane sisend'];
-    it('Submit form successfully', function() {
+    it('Submit form unsuccessfully', function() {
         cy.clearCookies();
         for (let i = 0; i < languages.length; i++) {
             cy.visit(languages[i]);
