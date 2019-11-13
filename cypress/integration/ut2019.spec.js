bb69526e9ea40f43329f9303721e5c61fcd7a7cb
Anneli Klamas <annelik98@gmail.com>
2019-11-13 23:09:25 +0200

    added estonian page test to ut2019.spec.js (exercise 3)

--- cypress/integration/ut2019.spec.js
+++ cypress/integration/ut2019.spec.js
@@ -2,13 +2,17 @@ describe('ut2019', function() {
 	const email = 'test.user@ut2019.ee';
 	const name = 'Test User';
 	const message = 'Custom message!';
+	const languages = ['https://ut2019.voog.com/contact', 'https://ut2019.voog.com/contact-1'];
+	const reply = ['Thank you!', 'Aitäh!'];
 	it('Submit form successfully', function() {
 		cy.clearCookies();
-		cy.visit('https://ut2019.voog.com/contact');
-		cy.get('.contact-name input').type(name);
-		cy.get('.contact-email input').type(email);
-		cy.get('.contact-message textarea').type(message);
-		cy.get('input[type="submit"]').click();
-		cy.get('.form_notice').should('have.text', 'Thank you!');
+		for (let i = 0; i < languages.length; i++) {
+			cy.visit(languages[i]);
+			cy.get('.contact-name input').type(name);
+			cy.get('.contact-email input').type(email);
+			cy.get('.contact-message textarea').type(message);
+			cy.get('input[type="submit"]').click();
+			cy.get('.form_notice').should('have.text', reply[i]);
+		}
 	});
 });
